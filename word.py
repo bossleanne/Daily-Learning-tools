27ad8936cda2b1539ef6521588a915ff523a00f8
Lea <leanneloves93@gmail.com>
2021-07-27 11:16:25 +0800

    final version of cp words

--- word.py
+++ word.py
@@ -1,28 +1,26 @@
 import pandas as pd
 import sys
-import pprint
+import re
+from collections import Counter
 
-def self_test(wordList):
+
+def self_test(w,wordList):
     text = ""
+    wordList = [x for x in wordList if x == x]
+    enter = []
     for word in wordList:
-        if str(word) != 'nan':
-            while text!= word:
-#                print(word[0]+'_'*(len(word)-2)+word[-1])
-                text = input()
-                if text == 'h':
-                    print(word)
-#                elif text == ' ':
-#                    print(word[0]+'_'*(len(word)-2)+word[-1])
-                elif text == 'q':
-                    sys.exit(0)
-                elif text == word:
-#                    print('')
-                    break
-                else:
-                    print(word[0]+'_'*(len(word)-2)+word[-1])
-                
-#                    print(' '*len(word),"| Wrong |  ",word[0]+'_'*(len(word)-2)+word[-1])
-#                    print()
+        while text!= word:
+            text = input()
+            if text == 'h':
+                print(word)
+            elif text == 'q':
+                sys.exit(0)
+            elif text == word:
+                enter.append(text)
+                print(w,'\n',len(enter),'-',len(wordList),enter)
+                break
+            else:
+                print(word[0]+'_'*(len(word)-2)+word[-1])
 
 def print_key_value(dct):
     for item, amount in dct.items():  # dct.iteritems() in Python 2
@@ -37,13 +35,22 @@ def main():
     print_key_value(headingDic)
 
     test = input('Enter your choice: ').lower()
+
     if test == 'y':
         for k,v in wordDic.items():
             print(k)
-            self_test(v)
+            self_test(k,v)
+
+    elif re.search("[+-]", test):
+        twoRange = test.split('-')
+        for i in range(int(twoRange[0]),int(twoRange[-1])+1):
+            w = headingDic[i]
+            self_test(w, wordDic[w])
+
     else:
+        test = int(test)-1
         w = headingDic[int(test)]
-        self_test(wordDic[w])
+        self_test(w,wordDic[w])
 
 if __name__ == "__main__":
     main()
